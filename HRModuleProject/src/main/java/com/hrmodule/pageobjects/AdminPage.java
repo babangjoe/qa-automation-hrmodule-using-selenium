b0e88f58e5206e1808055486fac65861a0b74006
babangjoe <tutupolij.johan.ferdian@gmail.com>
2023-09-17 23:40:31 +0700

    add user

--- HRModuleProject/src/main/java/com/hrmodule/pageobjects/AdminPage.java
+++ HRModuleProject/src/main/java/com/hrmodule/pageobjects/AdminPage.java
@@ -3,9 +3,36 @@
  */
 package com.hrmodule.pageobjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.hrmodule.actiondriver.Action;
+import com.hrmodule.base.BaseClass;
+
 /**
  * 
  */
-public class AdminPage {
-
+public class AdminPage extends BaseClass {
+	@FindBy(xpath="//*[@class='oxd-main-menu']/li/a/*[text() = 'Admin']")
+	WebElement menuAdmin;
+	
+	@FindBy(xpath="//span[contains(text(), 'User Management')]")
+	WebElement btnUserManagement;
+	
+	@FindBy(xpath="//span[contains(text(), 'Job')]")
+	WebElement btnJob;
+	
+	public AdminPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public UserManagement clickOnUserManagement() {
+		Action.click(driver, menuAdmin);
+		Action.implicitWait(driver, 3);
+		Action.click(driver, btnUserManagement);
+		
+		return new UserManagement();
+	}
+	
 }
